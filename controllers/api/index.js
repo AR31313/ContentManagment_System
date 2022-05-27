27ebfa5168b8a0eacc930181b3d6a05ff4622f50
AR31313 <gurlgotgame@gmail.com>
2022-05-27 11:41:45 -0700

    routes and files updated

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const postRoutes = require('./blostPostRoutes');
 
 router.use('/users', userRoutes);
+router.use('/blogPost', postRoutes);
 
 module.exports = router;
